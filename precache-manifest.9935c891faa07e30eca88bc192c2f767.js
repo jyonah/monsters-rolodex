95883eabb15d430c50fef0b4bc88f807d1564960
Jonah Price <jonah.price@thoughtworks.com>
2019-07-17 14:25:06 -0500

    Updates

---
+++ precache-manifest.9935c891faa07e30eca88bc192c2f767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d2cd41af57c0045be39384dae87e54",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cd553b17570f3abd5ae9",
+    "url": "/monsters-rolodex/static/css/main.bc14a662.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cd553b17570f3abd5ae9",
+    "url": "/monsters-rolodex/static/js/main.cc73c48c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
